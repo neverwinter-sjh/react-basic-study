e084132302bfaafbc58db4bcd01cf0266bf3d6db
neverwinter <neverwinter@amorepacific.com>
2020-07-10 16:21:20 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import CounterContainer from 'src/containers/CounterContainer';
-import TodosContainer from 'src/containers/TodoContainer';
 
 function App() {
   return (
     <div>
-      <CounterContainer />      
+      <CounterContainer />
     </div>
   );
 }
