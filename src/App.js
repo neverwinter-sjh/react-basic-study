2d0785b3dd2b00b69a8c3d37d4a902e26f5e57b0
neverwinter <neverwinter@amorepacific.com>
2020-07-14 10:33:00 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 // import React, { useRef, useEffect, useState } from 'react';
-import React from 'react';
+import React, { Component } from 'react';
 // import CounterContainer from 'src/containers/CounterContainer';
 // import EventPractice from 'src/components/EventPractice';
 // import ValidationSample from 'src/components/ValidationSample';
 // import ScrollBox from 'src/components/ScrollBox';
-import IterationSample from 'src/components/IterationSample';
+// import IterationSample from 'src/components/IterationSample';
+import LifeCycleSample from 'src/components/LifeCycleSample';
+import ErrorBoundry from 'src/components/ErrorBoundry';
 
 // Functional Component에서 ref 사용하기
 // const App = React.forwardRef((props, ref) => {
@@ -31,12 +33,32 @@ import IterationSample from 'src/components/IterationSample';
 //   );
 // });
 
-const App = () => {
-  return (
-    <div>
-      <IterationSample />
-    </div>
-  );
-};
+function getRamdomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 
+class App extends Component {
+  state = {
+    color: '#000000'
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRamdomColor()
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <button type="button" onClick={this.handleClick}>
+          랜덤 색상
+        </button>
+        <ErrorBoundry>
+          <LifeCycleSample color={this.state.color} />
+        </ErrorBoundry>
+      </div>
+    );
+  }
+}
 export default App;
